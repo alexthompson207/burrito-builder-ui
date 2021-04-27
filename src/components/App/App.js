d4d1600bc0d11fdfa07cdf26de5ca88611092e43
Alex Thompson <ahthomps1@gmail.com>
2021-04-27 09:19:02 -0600

    Add state to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import { getOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
-  constructor(props) {
+  constructor() {
     super();
+    this.state = {
+      orders: []
+    }
   }
 
   componentDidMount() {
@@ -22,7 +25,7 @@ class App extends Component {
           <OrderForm />
         </header>
 
-        <Orders orders={this.state.orders}/>
+        <Orders orders={this.state.orders} />
       </main>
     );
   }
