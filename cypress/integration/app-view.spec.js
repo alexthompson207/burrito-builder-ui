71acd70dedb3dae3146147b7067446fb1325f95b
Alex Thompson <ahthomps1@gmail.com>
2021-04-27 10:32:44 -0600

    Fix bug of intercept of GET request

--- cypress/integration/app-view.spec.js
+++ cypress/integration/app-view.spec.js
@@ -2,18 +2,24 @@ describe('Burrito Builder Homepage', () => {
 
   beforeEach(() => {
 
-    crypto.intercept({
+    cy.intercept({
       method: 'GET',
       url: 'http://localhost:3001/api/v1/orders'
     },
       {
         statusCode: 201,
-        body: [
-          { id: 1, name: 'Worm', ingredients: ['beans', 'lettuce', 'carnitas', 'queso fresco', 'jalapeno'] },
-          { id: 2, name: 'Fish', ingredients: ['beans', 'hot sauce', 'steak', 'sour cream'] },
-          { id: 3, name: 'Doggy', ingredients: ['lettuce', 'guacamole', 'queso fresco'] }
-        ]
+        body: {
+          orders: [
+            { id: 1, name: 'Worm', ingredients: ['beans', 'lettuce', 'carnitas', 'queso fresco', 'jalapeno'] },
+            { id: 2, name: 'Fish', ingredients: ['beans', 'hot sauce', 'steak', 'sour cream'] },
+            { id: 3, name: 'Doggy', ingredients: ['lettuce', 'guacamole', 'queso fresco'] }
+          ]
+        }
       });
     cy.visit('http://localhost:3000');
-  })
+  });
+
+  it('Should be able to visit the page and view the title', () => {
+    cy.get('h1').contains('Burrito Builder');
+  });
 })
